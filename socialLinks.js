ca44865bce7f363d1a4ea17e738b08653dcb3ad7
Csp-Ai <poncechris95@gmail.com>
2025-07-30 23:50:09 -0700

    Update social links and add config

---
+++ socialLinks.js
@@ -0,0 +1,5 @@
+export const SOCIAL_LINKS = {
+  instagram: 'https://www.instagram.com/hybriddancers/',
+  tiktok: 'https://www.tiktok.com/@hybrid.dancers',
+  facebook: 'https://www.facebook.com/people/Hybrid-Dancers/61577357196451/'
+};
